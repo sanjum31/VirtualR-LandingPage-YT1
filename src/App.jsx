fb5063995a7a0fd2cb692eaeeb2a72906180d8f2
sanjum31 <sadiaanjum3128@gmail.com>
2024-09-02 11:04:31 +0600

    Landing page completed

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+function App() {
+
+
+  return (
+    <>
+  <Navbar></Navbar>
+  <div className='max-w-7xl mx-auto pt-20 px-6'>
+  <HeroSection></HeroSection>
+  <FeatureSection></FeatureSection>
+  <WorkFlow></WorkFlow>
+  <Pricing></Pricing>
+  <Testimonials></Testimonials>
+
+  </div>
+  <Footer></Footer>
+ 
+    </>
+  )
+}
+
+export default App
